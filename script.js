17046119105997a1cd1e5e2f916a9d456e499ab0
Farheen Shadani <49196144+Farheen-Shadani@users.noreply.github.com>
2019-09-03 12:21:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+// Make a request for a user with a given ID
+axios.get('https://tk-whatsapp.herokuapp.com/messages')
+    .then(function (response) {
+        // handle success
+        console.log(response);
+        for (let i = 0; i < response.data.length; i++) {
+            let message = response.data[i];
+            document.querySelector("ul").innerHTML += `
+            <li>
+                <img src="${message.profilePic}">
+                <div class="messages">
+                    <div class="info">
+                        <h3>${message.name}</h3>
+                        <p class="time">${message.time.toLowerCase()}</p>
+                    </div>
+                    <div class="details">
+                        <p class="text">${message.firstLine}</p>
+                        <p class="numb">${message.numbUnread}</p>
+                    </div>
+                </div>
+            </li>
+            `;
+            // if (this.numbUnread >= 0) {
+            //     document.querySelector('.time').style.color = "#07d260";
+            // } else {
+            //     document.querySelector('.numb').style.display = "none";
+            // };
+        }
+    }
+    )
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    })
+    .finally(function () {
+        // always executed
+    });
